410f5f7c5317c1e7b40ea4d7d48e7435b526ebff
Helen <helenlu66@gmail.com>
2026-06-04 11:50:43 -0700

    removed cardinal direction condition

--- prompting_experiments/exp_design.py
+++ prompting_experiments/exp_design.py
@@ -14,9 +14,8 @@ if TYPE_CHECKING:
 from .condition_set_1_prompt import CONDITION_SET as CONDITION_SET_1
 from .condition_set_2_observation_format import CONDITION_SET as CONDITION_SET_2
 from .condition_set_3_context_window import CONDITION_SET as CONDITION_SET_3
-from .condition_set_4_action_space import CONDITION_SET as CONDITION_SET_4
-from .condition_set_5_querying_strategy import CONDITION_SET as CONDITION_SET_5
-from .condition_set_6_in_context_learning import CONDITION_SET as CONDITION_SET_6
+from .condition_set_4_querying_strategy import CONDITION_SET as CONDITION_SET_5
+from .condition_set_5_in_context_learning import CONDITION_SET as CONDITION_SET_6
 from .core import ConditionSet, Variant, iter_condition_configs as _iter_condition_configs
 
 
@@ -24,7 +23,6 @@ CONDITION_SETS: Mapping[str, ConditionSet] = {
     CONDITION_SET_1.name: CONDITION_SET_1,
     CONDITION_SET_2.name: CONDITION_SET_2,
     CONDITION_SET_3.name: CONDITION_SET_3,
-    CONDITION_SET_4.name: CONDITION_SET_4,
     CONDITION_SET_5.name: CONDITION_SET_5,
     CONDITION_SET_6.name: CONDITION_SET_6,
 }
